8e6e81e7628eede7106c9e840589f124634d5111
John Else <john.else@citrix.com>
2015-09-17 17:01:50 +0100

    Improve the test script
    Start the container, wget a test file, and check it has the expected
    contents.
    
    Signed-off-by: John Else <john.else@citrix.com>

--- test.sh
+++ test.sh
@@ -3,3 +3,12 @@
 set -eux
 
 make
+
+mkdir -p /tmp/lighttpd-test
+echo "test123" > /tmp/lighttpd-test/index.html
+
+./run.py --address 127.0.0.1 --port 8080 /tmp/lighttpd-test
+
+wget 127.0.0.1:8080
+
+diff /tmp/lighttpd-test/index.html index.html
