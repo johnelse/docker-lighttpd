2960c85fad825b6a12fe8e85e586c0e5bf8b98e9
John Else <john.else@citrix.com>
2015-10-21 09:09:42 +0100

    Test that the container can be stopped and rm'd
    Signed-off-by: John Else <john.else@citrix.com>

--- test.sh
+++ test.sh
@@ -7,8 +7,11 @@ make
 mkdir -p /tmp/lighttpd-test
 echo "test123" > /tmp/lighttpd-test/index.html
 
-./run.py --address 127.0.0.1 --port 8080 /tmp/lighttpd-test
+CONTAINER=`./run.py --address 127.0.0.1 --port 8080 /tmp/lighttpd-test`
 
 wget 127.0.0.1:8080
 
 diff /tmp/lighttpd-test/index.html index.html
+
+docker stop $CONTAINER
+docker rm $CONTAINER
